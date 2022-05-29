36ca6ce15c26a74d1e812f4fff81af9baf148f9b
Studio Kaori <studiokaori.com@gmail.com>
2022-05-29 11:38:11 +0200

    Initial Commit

---
+++ SwiftUIFruits/SwiftUIFruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFruitsApp.swift
+//  SwiftUIFruits
+//
+//  Created by Kaori Persson on 2022-05-29.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
