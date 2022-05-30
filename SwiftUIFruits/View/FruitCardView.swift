76ac171f36ec2d0ca8f03c6908afa75ec8b62c63
Studio Kaori <studiokaori.com@gmail.com>
2022-05-30 06:49:19 +0200

    Organise: folders

---
+++ SwiftUIFruits/View/FruitCardView.swift
@@ -0,0 +1,20 @@
+//
+//  FruitCardView.swift
+//  SwiftUIFruits
+//
+//  Created by Kaori Persson on 2022-05-30.
+//
+
+import SwiftUI
+
+struct FruitCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FruitCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitCardView()
+    }
+}
